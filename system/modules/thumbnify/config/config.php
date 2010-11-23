876a2edafeeb69e0181bf7255778498dfd8a1db5
Tristan Lins <tristan.lins@infinitysoft.de>
2010-11-23 15:49:19 +0000

    Initial import.

---
+++ system/modules/thumbnify/config/config.php
@@ -0,0 +1,44 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  InfinitySoft 2010
+ * @author     Tristan Lins <tristan.lins@infinitysoft.de>
+ * @package    Thumbnify
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/**
+ * Constants
+ */
+define('WATERMARK_LEFT_TOP', 'NorthWest');
+define('WATERMARK_TOP', 'North');
+define('WATERMARK_RIGHT_TOP', 'NorthEast');
+define('WATERMARK_LEFT', 'West');
+define('WATERMARK_CENTER', 'Center');
+define('WATERMARK_RIGHT', 'East');
+define('WATERMARK_LEFT_BOTTOM', 'SouthWest');
+define('WATERMARK_BOTTOM', 'South');
+define('WATERMARK_RIGHT_BOTTOM', 'SouthEast');
+
+?>
